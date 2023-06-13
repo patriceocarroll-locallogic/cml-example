63330195709875945d2d9064be0c6703b6695aae
Patrice O'Carroll <patrice.ocarroll@locallogic.co>
2023-06-13 13:49:59 -0400

    LL22-2097 feat(mlops): tracking data, env and code

--- train.py
+++ train.py
@@ -2,25 +2,30 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import numpy as np
-
+import mlflow
 # Read in data
 X_train = np.genfromtxt("data/train_features.csv")
 y_train = np.genfromtxt("data/train_labels.csv")
 X_test = np.genfromtxt("data/test_features.csv")
 y_test = np.genfromtxt("data/test_labels.csv")
 
-# Fit a model
-depth = 5
-clf = RandomForestClassifier(max_depth=depth)
-clf.fit(X_train, y_train)
+with mlflow.start_run() as run:
+    # Fit a model
+    depth = 6
+    clf = RandomForestClassifier(max_depth=depth)
+    clf.fit(X_train, y_train)
 
-acc = clf.score(X_test, y_test)
-print(acc)
-with open("metrics.txt", "w") as outfile:
-    outfile.write("Accuracy: " + str(acc) + "\n")
+    acc = clf.score(X_test, y_test)
+    print(acc)
+    with open("metrics.txt", "w") as outfile:
+        outfile.write("Accuracy: " + str(acc) + "\n")
 
-# Plot it
-disp = ConfusionMatrixDisplay.from_estimator(
-    clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues
-)
-plt.savefig("plot.png")
+    # Plot it
+    disp = ConfusionMatrixDisplay.from_estimator(
+        clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues
+    )
+    plt.savefig("confusion_matrix.png")
+    mlflow.log_param("max_depth", "depth")
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_artifact(local_path="confusion_matrix.png", artifact_path="figures")
+
